9d5abecf18f574317f28a86a53cff0ce15ee29fd
jh.baek <JH-BK@robotics.snu.ac.kr>
2022-04-14 00:30:19 +0900

    Feat: Add Stop Loss stretegy

--- auto_trade.py
+++ auto_trade.py
@@ -107,11 +107,13 @@ def get_stock_balance(code):
     cpBalance.SetInputValue(2, 50)          # 요청 건수(최대 50)
     cpBalance.BlockRequest()
     if code == 'ALL':
-        dbgout('계좌명: ' + str(cpBalance.GetHeaderValue(0)))
-        dbgout('결제잔고수량 : ' + str(cpBalance.GetHeaderValue(1)))
-        dbgout('평가금액: ' + str(cpBalance.GetHeaderValue(3)))
-        dbgout('평가손익: ' + str(cpBalance.GetHeaderValue(4)))
-        dbgout('종목수: ' + str(cpBalance.GetHeaderValue(7)))
+        dbgout(
+            f'계좌명: {cpBalance.GetHeaderValue(0)}\n'
+            f'결제잔고수량 : {cpBalance.GetHeaderValue(1)}\n'
+            f'평가금액: {cpBalance.GetHeaderValue(3)}\n'
+            f'평가손익: {cpBalance.GetHeaderValue(4)}\n'
+            f'종목수: {cpBalance.GetHeaderValue(7)}\n'
+        )
     stocks = []
     for i in range(cpBalance.GetHeaderValue(7)):
         stock_code = cpBalance.GetDataValue(12, i)  # 종목코드
@@ -157,7 +159,7 @@ def get_target_price(code):
             today_open = lastday[3]
         lastday_high = lastday[1]
         lastday_low = lastday[2]
-        target_price = today_open + (lastday_high - lastday_low) * 0.5
+        target_price = today_open + (lastday_high - lastday_low) * 0.3
         return target_price
     except Exception as ex:
         dbgout("`get_target_price() -> exception! " + str(ex) + "`")
@@ -182,12 +184,50 @@ def get_movingaverage(code, window):
         return None
 
 
+def check_loss_and_sell_etf(code, loss: float = 0.005):
+    """Check loss condition and sell all when the condition is met."""
+
+    try:
+        global bought_list
+        if code not in bought_list:
+            return False
+
+        stock_name, stock_qty = get_stock_balance(code)
+        current_price, ask_price, bid_price = get_current_price(code)
+        target_price = get_target_price(code)    # 매수 목표가
+
+        if current_price < target_price * (1.0 - loss):
+            printlog(
+                f"{stock_name}({code}){stock_qty}EA : {current_price} meets the stop loss condition!`"
+            )
+            sell_etf(code)
+
+            # Additional check
+            if code not in bought_list:
+                printlog(
+                    f"{stock_name}({code}){stock_qty}EA : {current_price}"
+                    " sold out due to the loss condition!`"
+                )
+            else:
+                stock_name, remaining_stock_qty = get_stock_balance(code)
+                printlog(
+                    f"{stock_name}({code}){stock_qty}EA : {current_price}"
+                    f" did not sold out clearly. Total {remaining_stock_qty}EA remaining.`"
+                )
+            return True
+        else:
+            return False
+
+    except Exception as ex:
+        dbgout("`check_etf(" + str(code) + ") -> exception! " + str(ex) + "`")
+        return False
+
+
 def buy_etf(code):
     """인자로 받은 종목을 최유리 지정가 FOK 조건으로 매수한다."""
     try:
         global bought_list      # 함수 내에서 값 변경을 하기 위해 global로 지정
         if code in bought_list:  # 매수 완료 종목이면 더 이상 안 사도록 함수 종료
-            # printlog('code:', code, 'in', bought_list)
             return False
         time_now = datetime.now()
         current_price, ask_price, bid_price = get_current_price(code)
@@ -198,8 +238,7 @@ def buy_etf(code):
         if ask_price > 0:  # 매도호가가 존재하면
             buy_qty = buy_amount // ask_price
         stock_name, stock_qty = get_stock_balance(code)  # 종목명과 보유수량 조회
-        # printlog('bought_list:', bought_list, 'len(bought_list):',
-        #    len(bought_list), 'target_buy_count:', target_buy_count)
+
         if current_price > target_price and current_price > ma5_price \
                 and current_price > ma10_price:
             printlog(
@@ -274,16 +313,59 @@ def sell_all():
         dbgout("sell_all() -> exception! " + str(ex))
 
 
+def sell_etf(code):
+    """보유한 종목을 최유리 지정가 IOC 조건으로 매도한다."""
+    try:
+        global bought_list
+        if code not in bought_list:
+            return False
+
+        cpTradeUtil.TradeInit()
+        acc = cpTradeUtil.AccountNumber[0]       # 계좌번호
+        accFlag = cpTradeUtil.GoodsList(acc, 1)  # -1:전체, 1:주식, 2:선물/옵션
+        while True:
+            stock_name, stock_qty = get_stock_balance(code)
+            if stock_qty == 0:
+                bought_list.remove(code)
+                dbgout(
+                    f"`sell_etf({stock_name} : {code}"
+                    f") -> All sold out!`"
+                )
+                return True
+
+            if stock_qty != 0:
+                cpOrder.SetInputValue(0, "1")         # 1:매도, 2:매수
+                cpOrder.SetInputValue(1, acc)         # 계좌번호
+                cpOrder.SetInputValue(2, accFlag[0])  # 주식상품 중 첫번째
+                cpOrder.SetInputValue(3, code)   # 종목코드
+                cpOrder.SetInputValue(4, stock_qty)    # 매도수량
+                cpOrder.SetInputValue(7, "1")   # 조건 0:기본, 1:IOC, 2:FOK
+                cpOrder.SetInputValue(8, "12")  # 호가 12:최유리, 13:최우선
+                # 최유리 IOC 매도 주문 요청
+                ret = cpOrder.BlockRequest()
+                printlog('최유리 IOC 매도', code, stock_name, stock_qty,
+                         '-> cpOrder.BlockRequest() -> returned', ret)
+                if ret == 4:
+                    remain_time = cpStatus.LimitRequestRemainTime
+                    printlog('주의: 연속 주문 제한, 대기시간:', remain_time / 1000)
+            time.sleep(1)
+            time.sleep(30)
+    except Exception as ex:
+        dbgout("sell_etf() -> exception! " + str(ex))
+
+
 if __name__ == '__main__':
     try:
-        symbol_list = []
-        bought_list = []     # 매수 완료된 종목 리스트
-        target_buy_count = 3  # 매수할 종목 수
-        buy_percent = 0.1
+        symbol_list = []  # 매수 후보 종목 리스트
+        bought_list = []  # 매수 완료된 종목 리스트
 
         with open("./resources/ticker_list.txt", "r") as file:
             while line := file.readline():
                 symbol_list.append(str(line).rstrip('\n'))
+
+        target_buy_count = 5  # 매수할 종목 수
+        buy_percent = 0.25
+        stop_loss = 0.005  # target price에서 stop_loss 이상 손실 시 바로 매도
 
         printlog('check_creon_system() :', check_creon_system())  # 크레온 접속 점검
         stocks = get_stock_balance('ALL')      # 보유한 모든 종목 조회
@@ -302,27 +384,44 @@ if __name__ == '__main__':
             t_sell = t_now.replace(hour=15, minute=15, second=0, microsecond=0)
             t_exit = t_now.replace(hour=15, minute=20, second=0, microsecond=0)
             today = datetime.today().weekday()
+
             if today == 5 or today == 6:  # 토요일이나 일요일이면 자동 종료
                 printlog('Today is', 'Saturday.' if today == 5 else 'Sunday.')
                 sys.exit(0)
+
             if t_9 < t_now < t_start and not soldout:
                 soldout = True
                 sell_all()
+
             if t_start < t_now < t_sell:  # AM 09:05 ~ PM 03:15 : 매수
+                # 주식 매수
                 for sym in symbol_list:
                     if len(bought_list) < target_buy_count:
                         buy_etf(sym)
                         time.sleep(1)
+
+                # Stop loss implementation.
+                for sym in symbol_list.copy():
+                    is_sold = check_loss_and_sell_etf(sym, stop_loss)
+                    # Do not buy again today.
+                    if is_sold:
+                        symbol_list.remove(sym)
+
+                # 주식 잔고 확인
                 if t_now.minute == 30 and 0 <= t_now.second <= 5:
                     get_stock_balance('ALL')
                     time.sleep(5)
+
             if t_sell < t_now < t_exit:  # PM 03:15 ~ PM 03:20 : 일괄 매도
                 if sell_all():
                     dbgout('`sell_all() returned True -> self-destructed!`')
                     sys.exit(0)
+
             if t_exit < t_now:  # PM 03:20 ~ :프로그램 종료
                 dbgout('`self-destructed!`')
                 sys.exit(0)
+
             time.sleep(3)
+
     except Exception as ex:
         dbgout('`main -> exception! ' + str(ex) + '`')

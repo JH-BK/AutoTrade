b8e6e0b0bb7ebadced070a3af409a3f7ca7af06b
jh.baek <JH-BK@robotics.snu.ac.kr>
2022-04-17 01:20:22 +0900

    Refactor: Minor refactoring

--- auto_connect.py
+++ auto_connect.py
@@ -5,17 +5,23 @@ from pywinauto import application
 import time
 import os
 
-os.system('taskkill /IM coStarter* /F /T')
-os.system('taskkill /IM CpStart* /F /T')
-os.system('taskkill /IM DibServer* /F /T')
-os.system('wmic process where "name like \'%coStarter%\'" call terminate')
-os.system('wmic process where "name like \'%CpStart%\'" call terminate')
-os.system('wmic process where "name like \'%DibServer%\'" call terminate')
-time.sleep(5)
 
-with open("./resources/app_start_code.txt", "r") as file:
-    app_start_code = file.readline()
+def connect_creon_api(code_dir="./resources/app_start_code.txt"):
+    os.system('taskkill /IM coStarter* /F /T')
+    os.system('taskkill /IM CpStart* /F /T')
+    os.system('taskkill /IM DibServer* /F /T')
+    os.system('wmic process where "name like \'%coStarter%\'" call terminate')
+    os.system('wmic process where "name like \'%CpStart%\'" call terminate')
+    os.system('wmic process where "name like \'%DibServer%\'" call terminate')
+    time.sleep(5)
 
-app = application.Application()
-app.start(app_start_code)
-time.sleep(60)
+    with open(code_dir, "r") as file:
+        app_start_code = file.readline()
+
+    app = application.Application()
+    app.start(app_start_code)
+
+
+if __name__ == "__main__":
+    connect_creon_api("./resources/app_start_code.txt")
+    time.sleep(30)

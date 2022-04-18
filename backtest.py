053f621eb0f4facf99a40963ed02264233a4d7c7
jh.baek <JH-BK@robotics.snu.ac.kr>
2022-04-19 00:04:54 +0900

    Refactor: Data features into data tools

--- backtest.py
+++ backtest.py
@@ -4,52 +4,26 @@ import datetime
 
 from data.data_tool import (
     check_creon_connection,
-    get_stockweek_object,
-    request_and_append_data,
+    request_recent_n_day_data
 )
 from stats.moving_average import simple_moving_average
 from visualize.chart import draw_stock_chart
 
-STOCK_CODE = 'A123310'
+STOCK_CODE = 'A122630'  # KODEX 레버리지
 INITIAL_SEED = 1000000
 STOCK_FEE_RATIO = 0.00015
 STOCK_SELL_FEE_RATIO = 0.0023
-USE_STOP_LOSS = False
-VOLATILITY_BREAK_K = 0.5
+USE_STOP_LOSS = True
+STOP_LOSS_RATIO = 0.005
+VOLATILITY_BREAK_K = 0.3
 
 if __name__ == "__main__":
 
     # API 연결 상태 확인
     check_creon_connection()
 
-    # 일자별 object 구하기
-    objStockWeek = get_stockweek_object(code=STOCK_CODE)
-
-    data_history_dict = {}
-
-    # 최초 데이터 요청
-    ret = request_and_append_data(objStockWeek, data_history_dict)
-    if not ret:
-        exit()
-
-    # 연속 데이터 요청
-    request_count = 0
-    while objStockWeek.Continue:  # 연속 조회처리
-        request_count += 1
-        if request_count > 5:
-            break
-        ret = request_and_append_data(objStockWeek, data_history_dict)
-        if not ret:
-            exit()
-
-    # 데이터 전처리
-    for k, v in data_history_dict.items():
-        if k in "date":
-            data_history_dict[k] = [
-                datetime.datetime.strptime(str(d), "%Y%m%d").date() for d in v
-            ]
-        else:
-            data_history_dict[k] = np.array(v[::-1])
+    # 지난 365일 종목 정보 불러오기
+    data_history_dict = request_recent_n_day_data(STOCK_CODE, 200)
 
     fig, (plt_chart, plt_seed) = plt.subplots(2, 1, gridspec_kw={'height_ratios': [3, 1]})
     plt_chart = draw_stock_chart(ax=plt_chart, **data_history_dict)
@@ -82,12 +56,12 @@ if __name__ == "__main__":
         # Algorithm here.
         conditions = [
             today_high > target_price,
-            today_high > simple_moving_average(10, today_idx, data_history_dict),
-            today_high > simple_moving_average(30, today_idx, data_history_dict)
+            today_high > simple_moving_average(5, today_idx, data_history_dict),
+            today_high > simple_moving_average(15, today_idx, data_history_dict),
         ]
         if (all(conditions)):
             buy_qty = np.floor(current_seed / buy_price).astype(int)
-            current_seed -= (buy_price * (1.0 + STOCK_FEE_RATIO)) * buy_qty
+            current_seed -= np.ceil(buy_price * (1.0 + STOCK_FEE_RATIO)) * buy_qty
             # print(
             #     f"Bought {buy_qty}EA with price {buy_price}"
             #     f" at {data_history_dict['date'][today_idx]}"
@@ -98,19 +72,22 @@ if __name__ == "__main__":
 
             # Stop loss
             if USE_STOP_LOSS:
-                if today_close < buy_price * 0.995:
-                    sell_price = buy_price * 0.995
+                if today_close < buy_price * (1 - STOP_LOSS_RATIO):
+                    sell_price = buy_price * (1 - STOP_LOSS_RATIO)
                     # print(f"Stop loss at {data_history_dict['date'][today_idx]}")
-                    is_profit_flag_list.append(False)
                 else:
                     sell_price = today_close
-                    is_profit_flag_list.append(True)
+                # if today_high > buy_price * 1.05:
+                #     sell_price = buy_price * 1.05
             else:
                 sell_price = today_close
-                if_profit = buy_price > sell_price
-                is_profit_flag_list.append(if_profit)
 
-            current_seed += (sell_price * (1.0 - STOCK_FEE_RATIO - STOCK_SELL_FEE_RATIO)) * buy_qty
+            if_profit = buy_price < sell_price
+            is_profit_flag_list.append(if_profit)
+
+            current_seed += np.floor(
+                sell_price * (1.0 - STOCK_FEE_RATIO - STOCK_SELL_FEE_RATIO)
+            ) * buy_qty
 
         total_seed_list.append(current_seed)
 
@@ -123,11 +100,22 @@ if __name__ == "__main__":
         marker="|", c=col, s=75,
     )
 
-    plt_seed.plot(data_history_dict["date"], np.array(total_seed_list) / INITIAL_SEED)
+    plt_seed.axhline(y=1.0, color='r', linestyle='--', alpha=0.7)
+    plt_seed.plot(
+        data_history_dict["date"],
+        np.array(total_seed_list) / INITIAL_SEED,
+        label="Balance"
+    )
+    plt_seed.plot(
+        data_history_dict["date"],
+        np.array(data_history_dict["open"]) / data_history_dict["open"][0],
+        alpha=0.7,
+        label="Stock price"
+    )
+    plt.legend()
 
-    fig.tight_layout()
     plt_chart.grid()
-    plt_chart.set_title("Stock chart")
+    plt_chart.set_title(f"Stock chart ({STOCK_CODE})")
     plt_chart.set_xlabel("Date")
     plt_chart.set_ylabel("Price (Won)")
 
@@ -135,5 +123,7 @@ if __name__ == "__main__":
     plt_seed.set_title("Seed balance")
     plt_seed.set_xlabel("Date")
     plt_seed.set_ylabel("Price (%)")
+
+    fig.tight_layout()
 
     plt.show()
